b495f6328da4b9fd469e5b0df134007b530608e8
Jesse <kamauj93@gmail.com>
2022-02-01 10:31:31 +0000

    Added first js file from given tasks

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+// console.log("Hello World");
+
+// let str1 = "Welcome to JavaScript";
+
+// console.log(str1);
+
+// console.info(str1);
+
+// console.warn(str1);
+
+// console.error(str1);
+
+// let car = "Audi A5 ";
+// let myObj = { str: "Just keep logging you are nearly there ", id:48576 }
+// console.info("My first car was a ", car, ". The object is: ", myObj);
+
+console.log("Jesse");
+
+console.info("Kamau");
+
+console.warn("Kenya");
+
+console.error("Sagiturus");
+
+let car1 = "Volkswagen";
+let model = "Golf GTI";
+
+console.log("My favourite car is " + car1 + " " + "and the model is " + model)
+
+let myTxt = "This could be useful!";
+
+console.log("this is a %c a message with some CSS."+myTxt,"color: orange; font-style:bold; font-family: fantasy; background-color: black;padding: 10px");
